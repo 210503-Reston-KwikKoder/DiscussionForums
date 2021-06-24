2055bf8c1d498321f304265bdde1b402a99d9b5b
Robert Fisher <RobertNFisherJR@gmail.com>
2021-06-24 14:57:09 -0400

    Update

--- DiscussionForum/DFDL/DFDBContext.cs
+++ DiscussionForum/DFDL/DFDBContext.cs
@@ -16,22 +16,14 @@ namespace DFDL
 
         }
         // Insert DBSets here for data models
-        public DbSet<DogList> DogLists { get; set; }
-        public DbSet<ListedDog> ListedDogs { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Like> Likes { get; set; }
         public DbSet<Comments> Comments { get; set; }
         public DbSet<Forum> Forums { get; set; }
         public DbSet<Posts> Posts { get; set; }
-        public DbSet<Preference> Preferences { get; set; }
-        public DbSet<Tags> Tags { get; set; }
-        public DbSet<Alert> Alert { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<DogList>()
-                .Property(list => list.ListID)
-                .ValueGeneratedOnAdd();
+
             modelBuilder.Entity<Comments>()
                 .Property(comm => comm.CommentID)
                 .ValueGeneratedOnAdd();
@@ -41,33 +33,6 @@ namespace DFDL
             modelBuilder.Entity<Posts>()
                 .Property(post => post.PostID)
                 .ValueGeneratedOnAdd();
-            modelBuilder.Entity<Tags>()
-                .Property(tag => tag.TagID)
-                .ValueGeneratedOnAdd();
-            modelBuilder.Entity<Alert>()
-                .Property(alert => alert.AlertID)
-                .ValueGeneratedOnAdd();
-
-
-            modelBuilder.Entity<User>()
-                .HasKey(user => user.UserID);
-            modelBuilder.Entity<ListedDog>()
-                .HasKey(LDog => new { LDog.ListID, LDog.APIID });
-            modelBuilder.Entity<DogList>()
-                .HasKey(list => list.ListID);
-            modelBuilder.Entity<Like>()
-                .HasKey(like => new { like.DogID, like.UserName });
-            modelBuilder.Entity<Comments>()
-                .HasKey(comm => comm.CommentID);
-            modelBuilder.Entity<Forum>()
-                .HasKey(forum => forum.ForumID);
-            modelBuilder.Entity<Posts>()
-                .HasKey(post => post.PostID);
-            modelBuilder.Entity<Preference>()
-                .HasKey(pref => new { pref.TagID, pref.UserName });
-            modelBuilder.Entity<Tags>()
-                .HasKey(tag => tag.TagID);
-
 
         }
     }
